84b22498d98bd1a1fc0534ffd85b269aedafa207
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-09-25 14:44:28 +0200

    Adding remapping function for mechanisms.

---
+++ R/remapMechs.R
@@ -0,0 +1,10 @@
+remap_mechs<-function(mechs_in,base.url,username,password,mode="code",ou) {
+  is_valid_mode<-mode %in% c("code","name")
+  if ( is_valid_mode == FALSE )  {break}
+  r<-GET(URLencode(paste0(base.url,"/api/categoryOptions?filter=organisationUnits.id:eq:",ou,"&fields=name,id,code,categoryOptionCombos[id]&filter=endDate:gt:2016-09-29&paging=false")),
+         authenticate(username,password))
+  r<- content(r, "parsed", "application/json")
+  mechs<-ldply(lapply(r$categoryOptions, function(x) t(unlist(x))))
+  mechs<-colwise(as.character)(mechs)
+  cmd<-paste0("mapvalues(mechs_in,mechs$",mode,",mechs$categoryOptionCombos.id,warn_missing = FALSE)")
+  eval(parse(text=cmd)) }
