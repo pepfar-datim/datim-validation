9148e9209dbe485dd0b5cfa1b8e454f6d8dbd484
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-09-30 15:50:54 +0200

    Adding pckgname required by roxygen2

---
+++ R/pkgname.R
@@ -0,0 +1,12 @@
+#' Generate R documentation from inline comments.
+#'
+#' Roxygen2 allows you to write documentation in comment blocks co-located
+#' with code.
+#'
+#' The only function you're likely to need from \pkg{roxygen2} is
+#' \code{\link{roxygenize}}. Otherwise refer to the vignettes to see
+#' how to format the documentation.
+#'
+#' @docType package
+#' @name datim-validation
+NULL
