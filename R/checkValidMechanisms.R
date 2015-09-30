f0e73bb740fabb6bc85f581ae0e6e3a2c68cdfde
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-09-30 15:50:17 +0200

    Adding documentation

--- R/checkValidMechanisms.R
+++ R/checkValidMechanisms.R
@@ -13,7 +13,7 @@
 #' checkValidMechanisms(foo,"https://www.datim.org","admin","district","Ab12345678")
 #' will remap organisation units specified as codes to UIDs
 checkValidMechanisms <-
-function(data,base.url,username,passowrd,organisationUnit) {
+function(data,base.url,username,password,organisationUnit) {
 r<-GET(URLencode(paste0(base.url,"api/categoryOptions?filter=organisationUnits.id:eq:",organisationUnit,"&fields=name,id,code,categoryOptionCombos[id]&filter=endDate:gt:2016-09-29&paging=false")),
        authenticate(username,password))
 r<- content(r, "parsed", "application/json")
