31e740dfbb9e56ecda273db1b1d0b4f72fd117a1
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-02 15:08:24 +0200

    Adding function to check the operating unit. Updated vignette.

---
+++ R/checkOperatingUnit.R
@@ -0,0 +1,22 @@
+#' @title Function which checks that the organisation unit provided is valid
+#' 
+#' @description Validates that the supplied UID is a valid operating unit UID
+#' 
+#' @param organisationUnit Should be a UID of an operating unit
+#' @param base.url Location of the server
+#' @param username Server username
+#' @param password Server password
+#' @return Returns a vector of organisation unit UIDs
+#' @note
+#' remapOUs(foo,"https://www.datim.org","admin","district","code","id","Ab12345678")
+#' will remap organisation units specified as codes to UIDs
+checkOperatingUnit<-function(organisationUnit,base.url,username,password) {
+
+       r<-GET(URLencode(paste0(base.url,"api/organisationUnits/api/organisationUnits?level=3&fields=id,name&paging=false")),
+              authenticate(username,password))
+       r<- content(r, "parsed", "application/json")
+       sites<-ldply(lapply(r$organisationUnits, function(x) t(unlist(x))))
+       sites<-colwise(as.character)(sites)
+       isValid<-organisationUnit %in% sites$id
+      return(isValid)       
+}
