bc0f226bbeec9470cbce69c65fd08f09fe06ec2b
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-10-23 14:06:12 +0200

    Adding missing files.

---
+++ R/getCategoryOptionCombosMap.R
@@ -0,0 +1,23 @@
+#' @title getCategoryOptionCombosMap(base.url,username,passowrd)
+#' 
+#' @description Utility function to produce a map of category option combos
+#'
+#' @param base.url Location of the server
+#' @param username Server username
+#' @param password Server password
+#' @return Returns a data frame  of code,name,id and shortName of all categoryOptionCombos
+
+getCategoryOptionCombosMap<-function(base.url,username,password) {
+  
+  r<-httr::GET(URLencode(paste0(base.url,"api/categoryOptionCombos?fields=id,name,shortName,code&paging=false")),
+             httr::authenticate(username,password),httr::timeout(60))
+  if (r$status == 200L ){
+    r<- httr::content(r, "text")
+    cocs<-jsonlite::fromJSON(r,flatten=TRUE)[[1]]
+    return( cocs ) } else {
+      print(paste("Could not retreive category option combos map",httr::content(r,"text")))
+      stop()
+    }
+  
+  
+}
---
+++ R/remapCategoryOptionCombos.R
@@ -0,0 +1,23 @@
+#' @title Function which converts category option combos from identifier to another 
+#' 
+#' @description remapCategoryOptionCombos should be supplied a vector of category option combos (names,codes,uids or shortnames)
+#' It will return a vector of another class as specified with the mode_out paramater
+#' 
+#'
+#' @param cocs_in A vector of category option combinations
+#' @param base.url Location of the server
+#' @param username Server username
+#' @param password Server password
+#' @param mode_in Should be one of code, name,shortName or id. This is the class we are mapping from.
+#' @param mode_out Should be one of code,name,shortName or id. This is the class we are mapping to..
+#' @return Returns a vector of category option combos of the mode_out type.
+#' @note
+#' remapCategoryOptionCombos(foo,"https://www.datim.org","admin","district","code","name")
+#' will remap categoryOptionCombos specified as codes to their corresponding names.
+
+remapCategoryOptionCombos<-function(cocs_in,base.url,username,password,mode_in,mode_out) {
+  is_valid_mode<- (mode_in %in% c("code","name","id","shortName") ) & (mode_out %in% c("code","name","id","shortName") )
+  if ( is_valid_mode == FALSE )  { print("Not a valid mode. Must be one of code,name,shortName or id"); stop() }
+  cocs<-getCategoryOptionCombosMap(base.url,username,password)
+  cmd<-paste("plyr::mapvalues(cocs_in,cocs$",mode_in,",cocs$",mode_out,",warn_missing = FALSE)")
+  as.character(eval(parse(text=cmd))) }
