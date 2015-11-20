df2d167718ec90c350daeeac65dfc9ed3e98f9a6
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-11-20 14:35:02 +0100

    Adding function to clear object cache.

--- R/internals.R
+++ R/internals.R
@@ -32,3 +32,15 @@ saveCachedObject<-function(toCache,sig,wd=paste0(getwd(),"/.R_Cache/")) {
   if (!file.exists(wd)) {dir.create(wd)}
   saveRDS(toCache,file=paste0(wd,sig))
 }
+
+#' @title clearCache(wd)
+#'
+#' @description Internal utility function to clear the object cache.
+#'
+#' @param wd Cache directory path. By default, working directory + ".R_Cache"
+#' @return Nothing.
+#'
+clearCache<-function(wd=paste0(getwd(),"/.R_Cache/")){
+resp<-readline(paste("Are you sure you want to delete the directory ", wd,"[Y/N]"))
+if (resp == "Y" ) { unlink(wd) } else { print("Aborting.") }
+}
