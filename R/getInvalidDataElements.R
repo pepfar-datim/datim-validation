712834e5fcedca9c44d2a149d3f3840fc968ef87
Jason P. Pickering <jason.p.pickering@gmail.com>
2016-03-01 16:14:59 +0100

    Adding new check for invalid data element /orgunit associations.

--- R/getInvalidDataElements.R
+++ R/getInvalidDataElements.R
@@ -12,51 +12,18 @@
 #' of invalid data elements which are present the the data 
 #'
 getInvalidDataElements<-function(data,base.url,username,password,dataset){
-  #Only support MER Results and targets for now. 
-  valid.datasets<-c("MER Targets","MER Results")
-  if (!(dataset %in% valid.datasets)) {print("Not a valid dataset group"); stop()}
-  #Valid data set assignments against the dataset
-  url<-URLencode(paste0(base.url,"api/sqlViews/bkJ3PteNu7A/data.json?paging=false"))
-  sig<-digest::digest(url,algo='md5', serialize = FALSE)
-  des<-getCachedObject(sig)
-  if (is.null(des)) {
-  r<-httr::GET(url , httr::authenticate(username,password),httr::timeout(60))
-  if (r$status == 200L ){
-  r<- httr::content(r, "text")
-  r<- jsonlite::fromJSON(r)
-  des<-as.data.frame(r$rows)
-  foo<-r$header
-  names(des)<-as.character(foo$name) 
-  saveCachedObject(des,sig)
-  } 
-  else {print("Could not get valid data elements"); stop()} }
-
-  #Datasets
-  url<-URLencode(paste0(base.url,"api/dataSets?filter=name:like:",dataset,"&fields=id,name"))
-  sig<-digest::digest(url,algo='md5', serialize = FALSE)
-  ds<-getCachedObject(sig)
   
-if (is.null(ds)){
-  r<-httr::GET(url, httr::authenticate(username,password),httr::timeout(60))
-  if (r$status == 200L ){
-  r<- httr::content(r, "text")
-  ds<-jsonlite::fromJSON(r)$dataSets 
-  saveCachedObject(ds,sig)
-  }
-  else {print("Could not get datasets");break()}
-}
-
-  des.this<-des[des$dataset %in% ds$name,]
-  des.this$combi<-paste0(des.this$dataelementuid,".",des.this$categoryoptioncombouid)
-  des.this<-plyr::colwise(as.character)(des.this)
+  des<-getValidDataElements(base.url,username,password,dataset)
+  des$combi<-paste0(des$dataelementuid,".",des$categoryoptioncombouid)
+  des<-plyr::colwise(as.character)(des)
   
   foo<-unique(data[,c("dataElement","categoryOptionCombo")])
   foo$combi<-paste0(foo$dataElement,".",foo$categoryOptionCombo)
-  foo<-foo[!(foo$combi %in% des.this$combi ),]
+  foo<-foo[!(foo$combi %in% des$combi ),]
   foo<-foo[complete.cases(foo),]
   #Get all data element names and uids
-  foo$dataElementName<-plyr::mapvalues(as.character(foo$dataElement),as.character(des.this$dataelementuid),as.character(des.this$shortname),warn_missing=FALSE)
-  foo$categoryOptionComboName<-plyr::mapvalues(foo$categoryOptionCombo,des.this$categoryoptioncombouid,as.character(des.this$categoryoptioncombo),warn_missing=FALSE)
+  foo$dataElementName<-remapDEs(foo$dataElement,base.url,username,password,mode_in="id",mode_out="shortName")
+  foo$categoryOptionComboName<-remapCategoryOptionCombos(foo$categoryOptionCombo,base.url,username,password,mode_in="id",mode_out="shortName")
   return(foo[,c("dataElementName","categoryOptionComboName","dataElement","categoryOptionCombo")])
 
 }
---
+++ R/getInvalidDatasetMembers.R
@@ -0,0 +1,74 @@
+#' @title getInvalidDatasetMembers(data,base.url,username,password,organisationUnit,dataset)
+#' 
+#' @description Returns a data frame of invalid data element /organisation unit combinations
+#' based on the current data set assignments
+#'
+#' @param base.url Location of the server
+#' @param username Server username
+#' @param password Server password
+#' @param dataset Should be a description of the dataset group, such as "MER Results", "MER Targets" or "SIMS"
+#' @param organisationUnit Should be the UID of the organisation unit ancestor.
+#' @return Returns a data frame of "data" which was submitted along for a reason of why the data is considered to be invalid.
+
+getDataElementsOrgunits<-function(base.url,username,password,organisationUnit,dataset) {
+  #Only support MER Results and targets for now. 
+  valid.datasets<-c("MER Targets","MER Results","SIMS")
+  if (!(dataset %in% valid.datasets)) {print("Not a valid dataset group"); stop()}
+  #Datasets
+  url<-URLencode(paste0(base.url,"api/dataSets?filter=name:like:",dataset,"&fields=id,name"))
+  sig<-digest::digest(paste0(url),algo='md5', serialize = FALSE)
+  ds<-getCachedObject(sig)
+  if(is.null(ds)){
+  r<-httr::GET(url , httr::authenticate(username,password),httr::timeout(60))
+  r<- httr::content(r, "text")
+  ds<-jsonlite::fromJSON(r,flatten=TRUE)$dataSets$id
+  saveCachedObject(ds,sig)
+  }
+  url1<-paste0(base.url,"api/organisationUnits?fields=id&paging=false&filter=path:like:",organisationUnit,"&filter=dataSets.id:in:[",paste(ds,sep="",collapse=","),"]")
+  url2<-paste0(base.url,"api/dataElements?fields=id&paging=false&filter=dataSets.id:in:[",paste(ds,sep="",collapse=","),"]")
+  sig<-digest::digest(paste(url1,url2,sep="",collapse=""),algo='md5', serialize = FALSE)
+  des_ous<-getCachedObject(sig)
+  if (is.null(des_ous)){
+      #DES
+      r<-httr::GET(url1 , httr::authenticate(username,password),httr::timeout(60))
+      r<- httr::content(r, "text")
+      ous<-jsonlite::fromJSON(r,flatten=TRUE)$organisationUnits$id
+      #OUs
+      r<-httr::GET(url2, httr::authenticate(username,password),httr::timeout(60))
+      r<- httr::content(r, "text")
+      des<-jsonlite::fromJSON(r,flatten=TRUE)$dataElements$id
+      des_ous<-list(ous=ous,des=des)
+      saveCachedObject(des_ous,sig)
+    }
+  
+  return(des_ous) }
+
+
+getInvalidOrgunitsFromDatasets<-function(data,username,password,organisationUnit,dataset) {
+  des_ous<-getDataElementsOrgunits(base.url,username,password,organisationUnit,dataset)
+  return(subset(data,!(orgUnit %in% des_ous$ous)))
+  }
+
+getInvalidDataElementFromDatasets<-function(data,username,password,organisationUnit,dataset) {
+  des_ous<-getDataElementsOrgunits(base.url,username,password,organisationUnit,dataset)
+  subset(data,!(dataElement %in% des_ous$des))
+}
+
+getInvalidDatasetMembers<-function(data,username,password,organisationUnit,dataset){
+  foo=data.frame(dataElement=character(),period=character(),orgUnit=character(),categoryOptionCombo=character(),
+                 attributeOptionCombo=character(),value=character(),type=character())
+  ous<-getInvalidOrgunitsFromDatasets(data,username,password,organisationUnit,dataset)
+  if (!is.null(ous) & nrow(ous) > 0) {
+    ous$type="ORGUNIT DOES NOT EXIST IN DATASETS"
+    foo<-rbind(foo,ous)
+  }
+  
+  des<-getInvalidDataElementFromDatasets(data,username,password,organisationUnit,dataset)
+  if (!is.null(des) & nrow(des) > 0) {
+    des$type="DATAELEMENT DOES NOT EXIST IN DATASETS"
+    foo<-rbind(foo,des)
+  }
+  
+  return(foo)
+}
+  
