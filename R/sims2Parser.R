a6b8bf247467e4a4d49552988d39bb9afd3095e9
Jason P. Pickering <jason.p.pickering@gmail.com>
2016-11-22 13:51:16 +0100

    Second cut of SIMS2parser.

--- R/sims2Parser.R
+++ R/sims2Parser.R
@@ -63,6 +63,12 @@ sims2Parser <-
           mode_in = orgUnitIdScheme,
           mode_out = "id"
         )
+      #Filter out any thing which does not correspond to the orgUnit mapping scheme
+      ou_non_match<-unique(data$orgUnit)[!(unique(data$orgUnit) %in% getOrganisationUnitMap()$id)]
+      if (length(ou_non_match > 0)) {
+        msg<-paste0("The following orgunits are not valid and will be removed",paste(ou_non_match,sep="",collapse=","))
+      }
+    data<-data[!(data$orgUnit %in% ou_non_match),]
     }
     if (dataElementIdScheme != "id") {
       data$dataElement <-
@@ -87,13 +93,13 @@ sims2Parser <-
     invalid <-
       function(x) {
         sapply(x, function(x) {
-          is.na(x) || missing(x) || x == ""
+          is.na(x) || missing(x) || x == "" || x=="NULL"
         })
       }
     invalid.rows <-
       apply(apply(data, 2, invalid), 1, sum) != 0 #Anything which is not complete.
     if ( sum(invalid.rows) ) {
-        paste(sum(invalid.rows),
+        msg<-paste(sum(invalid.rows),
               " rows are incomplete. Please check your file to ensure its correct.")
       warning(msg)
     }
@@ -101,26 +107,52 @@ sims2Parser <-
     if (sum(invalid.rows)) {
       data <- data[!invalid.rows, ]
     }
+    #Start to shift the data
     data_shifted<-data[0,]
     assessments<-unique(data[,c("period","orgUnit","attributeOptionCombo","assessmentid")])
-    assessments<-aggregate(. ~ period + orgUnit + attributeOptionCombo,data=assessments,length)
-    names(assessments)<-c("period","orgUnit","attributeOptionCombo","assessments")
+    assessments_ou_acoc<-aggregate(. ~ orgUnit + attributeOptionCombo,data=assessments[,-4],length)
+    #Possible collisions
+    assessments_ou_acoc_dups<-assessments_ou_acoc[assessments_ou_acoc$period > 1,]
+    asessments_collisions<-assessments[0,]
+    for (i in 1:nrow(assessments_ou_acoc_dups)) {
+      foo<-assessments_ou_acoc_dups[i,]
+      bar<-assessments[assessments$orgUnit==foo$orgUnit & assessments$attributeOptionCombo==foo$attributeOptionCombo,]
+      #Are there any duplicated dates?
 
-    for (i in 1:nrow(assessments)) {
+      if (sum(duplicated(bar$period)) > 0) {
+        dates<-as.Date(unique(strptime(bar$period,"%Y%m%d",tz = "UTC")))
+        start_date<-min(dates)
+        end_date<-max(dates)
+        #We need a minumum pool of dates
+        if ( (start_date - end_date) < nrow(bar) ) {
+          end_date<-start_date + nrow(bar)
+        }
+        possible_dates<-seq(start_date,end_date,by="day")
+        #Remove any dates which are already used
+        possible_dates<-possible_dates[!(possible_dates %in% dates)]
       
-      foo<-merge(data,assessments[i,c("period","orgUnit","attributeOptionCombo")],
-                 by=c("period","orgUnit","attributeOptionCombo"))
-      if (foo$assessments > 1) {
-      assessments_dates<-seq(strptime(foo$period,"%Y%m%d"), by = "day", length.out = foo$assessments)
-      assessment_ids<-sort(unique(bar$assessmentid))
-      assessments_shifted<-data.frame(assessmentid=assessment_ids,assessments_dates=assessments_dates)
-      bar<-merge(bar,assessments_shifted,by=c("assessmentid"))
-      bar$period<-format(bar$assessments_dates,"%Y%m%d")
-      bar<- bar[, c("dataElement","period","orgUnit","categoryOptionCombo","attributeOptionCombo","value")] }
-      data_shifted<-rbind(data_shifted,bar) 
-      #TODO: How to handle date collisions after shifting?
+    duplicated_dates<-which(duplicated(bar$period))
+    for (j in 1:length(duplicated_dates)) {
+      this_date<-as.Date(bar$period[duplicated_dates[j]],"%Y%m%d")
+      #Which date is closest?
+      date_distance<-as(abs(possible_dates-this_date),"integer")
+      replacement_date_n<-which(date_distance == min(date_distance))
+
+      bar$period[duplicated_dates[j]]<-format(possible_dates[replacement_date_n],"%Y%m%d")
+      #Remove it from the pool
+      possible_dates<-possible_dates[-replacement_date_n]
     }
-    
-    return(data_shifted)
-    
+    }
+    asessments_collisions<-rbind(asessments_collisions,bar) }
+    #Non-collisions
+
+    data_clear<-merge(data,assessments_ou_acoc[assessments_ou_acoc$period == 1,c("orgUnit","attributeOptionCombo")],
+                      by=c("orgUnit","attributeOptionCombo"))
+    data_not_clear<-merge(data,asessments_collisions,
+                          by=c("orgUnit","attributeOptionCombo","assessmentid"))
+    data_not_clear<-data_not_clear[,c("orgUnit","attributeOptionCombo","dataElement","period.y","categoryOptionCombo","value","assessmentid")]
+    names(data_not_clear)<-names(data_clear)
+    data_shifted<-rbind(data_clear,data_not_clear)
+    assertthat::assert_that(nrow(data) == nrow(data_shifted)
+   return(data_shifted)
 }
