37faf31abecbb35d342fc1da17370272537fba8c
Jason Pickering <jason.p.pickering@gmail.com>
2017-01-22 13:24:27 +0700

    Refactor mechanisms map method.

--- R/getMechanismsMap.R
+++ R/getMechanismsMap.R
@@ -14,7 +14,7 @@ getMechanismsMap<-function(organisationUnit=NA) {
   #Determine if we should filter by OU
   ou_filter= !( organisationUnit== "ybg3MO3hcf4"  )
   #Special cases when global OU is used, particularly for SIMS Import
-  url<-paste0(getOption("baseurl"),"api/categoryOptionCombos?filter=categoryCombo.id:eq:wUpfppgjEza&fields=code,name,id,categoryOptions[startDate,endDate]&paging=false")
+  url<-paste0(getOption("baseurl"),"api/categoryOptionCombos?filter=categoryCombo.id:eq:wUpfppgjEza&fields=code,name,id,categoryOptions[startDate,endDate,organisationUnits[id]]&paging=false&filter=categoryOptions.organisationUnits.id:!null")
   if  ( ou_filter ){
     url<-paste0(url,paste0("&filter=categoryOptions.organisationUnits.id:eq:",organisationUnit)) 
   }
@@ -28,10 +28,22 @@ getMechanismsMap<-function(organisationUnit=NA) {
   if (r$status == 200L ){
   r<- httr::content(r, "text")
   mechs<-jsonlite::fromJSON(r,flatten=TRUE)[[1]]
+  
+  foo<-mechs$categoryOptions
+  n<-length(foo)
+  mechs_details<-data.frame(matrix(as.character(""), nrow = n, ncol = 3),stringsAsFactors = FALSE)
+  names(mechs_details)<-c("startDate","endDate","organisationUnits")
+  for (i in 1:length(foo)) {
+    bar<-foo[i]
+    mechs_details$startDate[i]<- bar[[1]]$startDate[1]
+    mechs_details$endDate[i]<- bar[[1]]$endDate[1]
+    mechs_details$organisationUnits[i]<-bar[[1]]$organisationUnits[[1]]$id
+  }
+  
   #Need to unwind the dates
-  mechs$startDate<-as.Date(sapply(mechs$categoryOptions, function(x) ifelse(is.null(x$startDate),"1900-01-01",x$startDate)),"%Y-%m-%d")
-  mechs$endDate<-as.Date(sapply(mechs$categoryOptions, function(x) ifelse(is.null(x$endDate),"1900-01-01",x$endDate)),"%Y-%m-%d")
-  mechs<-mechs[,-which(names(mechs) == "categoryOptions")]
+  mechs_details$startDate<-as.Date(sapply(mechs_details$startDate, function(x) ifelse(is.null(x),"1900-01-01",x)),"%Y-%m-%d")
+  mechs_details$endDate<-as.Date(sapply(mechs_details$endDate, function(x) ifelse(is.null(x),"1900-01-01",x)),"%Y-%m-%d")
+  mechs<-cbind(mechs[,c("code","name","id")],mechs_details)
   saveCachedObject(mechs,sig) }
   return( mechs ) } 
   
