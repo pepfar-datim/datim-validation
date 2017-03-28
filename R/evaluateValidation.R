cd335c980bedbb5e54771ec23ffed85fe8f9058e
Jason Pickering <jason.p.pickering@gmail.com>
2017-03-28 17:07:36 -0400

    Bug fix for logical operators.

--- R/evaluateValidation.R
+++ R/evaluateValidation.R
@@ -27,8 +27,10 @@ evaluateValidation<-function(combis,values,vr,return_violations_only=TRUE) {
   #Get the matching rules
   matches$leftSide.expression<-plyr::mapvalues(matches$leftSide.expression,combis,values,warn_missing=FALSE)
   matches$leftSide.count<-stringr::str_count(matches$leftSide.expression,expression.pattern)
+  matches$leftSide.count<-matches$leftSide.ops-matches$leftSide.count
   matches$rightSide.expression<-plyr::mapvalues(matches$rightSide.expression,combis,values,warn_missing=FALSE)
   matches$rightSide.count<-stringr::str_count(matches$rightSide.expression,expression.pattern)
+  matches$rightSide.count<-matches$rightSide.ops-matches$rightSide.count
   #Keep rules which should  be evaluated
   keep_these_rules<-(matches$leftSide.missingValueStrategy == "SKIP_IF_ANY_VALUE_MISSING" & (matches$leftSide.ops != matches$leftSide.count)) | 
     (matches$rightSide.missingValueStrategy == "SKIP_IF_ANY_VALUE_MISSING" & (matches$rightSide.ops != matches$rightSide.count)) | 
@@ -40,29 +42,25 @@ evaluateValidation<-function(combis,values,vr,return_violations_only=TRUE) {
   matches<-matches[keep_these_rules,]
   if (nrow(matches) > 0 ) {
    #Special handling for logical operators
-   matches_ex<-matches[matches$operator == "|",]
+   matches_ex<-matches[matches$operator %in% c("|","&"),]
    #Do the sides have data?
    if ( nrow(matches_ex) > 0 )  {
-   matches_ex$leftSide.expression<-matches_ex$leftSide.ops != matches_ex$leftSide.count
-   matches_ex$rightSide.expression<-matches_ex$rightSide.ops != matches_ex$rightSide.count
-   matches_ex$formula <- paste0("xor(",matches_ex$leftSide.expression,",",matches_ex$rightSide.expression,")") }
+   #matches_ex$leftSide.expression<-matches_ex$leftSide.count > 0
+   #matches_ex$rightSide.expression<-matches_ex$rightSide.count > 0
+   matches_ex$formula <- paste0(as.character(matches_ex$leftSide.count == 0), matches_ex$operator,as.character(matches_ex$rightSide.count == 0)) }  else { matches_ex<-validation.results_empty }
    
-   matches_comp<-matches[matches$operator == "&",]
-   #Do the sides have data?
-   if (nrow(matches_comp) > 0 ) {
-   matches_comp$leftSide.expression<-matches_comp$leftSide.ops != matches_comp$leftSide.count
-   matches_comp$rightSide.expression<-matches_comp$rightSide.ops != matches_comp$rightSide.count
-   matches_comp$formula == paste0(matches_comp$leftSide.expression,"&",matches_comp$rightSide.expression) }
-  
   #Normal operators
+  matches_normal<-matches[!(matches$operator %in% c("&","|")),]
+  if (nrow(matches_normal > 0)) {
+  matches_normal$leftSide.expression<-gsub(expression.pattern,"0",matches_normal$leftSide.expression) 
+  matches_normal$rightSide.expression<-gsub(expression.pattern,"0",matches_normal$rightSide.expression)
+  matches_normal$leftSide.expression<-sapply(matches_normal$leftSide.expression,function(x) {eval(parse(text=x))})
+  matches_normal$rightSide.expression<-sapply(matches_normal$rightSide.expression,function(x) {eval(parse(text=x))})
+  matches_normal$formula<-paste(matches_normal$leftSide.expression,matches_normal$operator,matches_normal$rightSide.expression) } else { matches_normal<-validation.results_empty}
   
-  if (nrow(matches > 0)) {
-  matches$leftSide.expression<-gsub(expression.pattern,"0",matches$leftSide.expression) 
-  matches$rightSide.expression<-gsub(expression.pattern,"0",matches$rightSide.expression)
-  matches$leftSide.expression<-sapply(matches$leftSide.expression,function(x) {eval(parse(text=x))})
-  matches$rightSide.expression<-sapply(matches$rightSide.expression,function(x) {eval(parse(text=x))})
-  matches$formula<-paste(matches$leftSide.expression,matches$operator,matches$rightSide.expression) }
-  matches<-rbind(matches,matches_comp,matches_ex)
+  matches<-rbind(matches_normal,matches_ex)
+  
+  
   matches$result<-vapply(matches$formula,function(x) {eval(parse(text=x))},FUN.VALUE=logical(1)) 
   if (return_violations_only == TRUE) { matches<-matches[!matches$result,] } 
   return( matches) } else
