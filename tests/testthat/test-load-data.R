1b3fa34a956a30376e9009331b524b77472971d4
Jason P. Pickering <jason.p.pickering@gmail.com>
2018-12-18 16:45:05 +0100

    Add additional tests for invalid metadata identifiers.

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -188,3 +188,35 @@ with_mock_api({
                 idScheme = "id",
                 invalidData = FALSE), "The following org unit identifiers could not be found:SiuNE0ywCW4")
   })})
+
+context("Can error on an invalid data element UID")
+
+with_mock_api({
+  test_that("We can create an error for an invalid data element identifier", {
+    config <- LoadConfigFile(test_config("test-config.json"))
+    options("maxCacheAge"=NULL)
+    expect_type(config,"list")
+    expect_error(d2Parser(filename=test_config("test-data-bad-de-uid.csv"),
+                          type="csv",
+                          organisationUnit = "KKFzPM8LoXs",
+                          dataElementIdScheme = "id",
+                          orgUnitIdScheme = "id",
+                          idScheme = "id",
+                          invalidData = FALSE), "The following data element identifiers could not be found:SiuNE0ywCW4")
+  })})
+
+context("Can error on an invalid attribute option combo UID")
+
+with_mock_api({
+  test_that("We can create an error for an invalid attribute option combo identifier", {
+    config <- LoadConfigFile(test_config("test-config.json"))
+    options("maxCacheAge"=NULL)
+    expect_type(config,"list")
+    expect_error(d2Parser(filename=test_config("test-data-bad-acoc-uid.csv"),
+                          type="csv",
+                          organisationUnit = "KKFzPM8LoXs",
+                          dataElementIdScheme = "id",
+                          orgUnitIdScheme = "id",
+                          idScheme = "id",
+                          invalidData = FALSE), "The following attribute option combo identifiers could not be found:SiuNE0ywCW4")
+  })})
