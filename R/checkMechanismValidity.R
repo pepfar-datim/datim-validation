257c27864781889440434aa9b4864d1d92defbe5
Jason P. Pickering <jason.p.pickering@gmail.com>
2018-12-19 07:40:14 +0100

    Consolidate mechanism checks into a single call.

---
+++ R/checkMechanismValidity.R
@@ -0,0 +1,56 @@
+#' @export
+#' @title checkMechanismPeriodValidity(data,organisationUnit)
+#' 
+#' @description This function will return an object of invalid mechanisms and periods. All data which is reported 
+#' must have a period within the valid start and end dates of the attribute option combination to which it is assigned. 
+#' The mechanism must also be associated with the operating unit. If either of these two conditions 
+#' are not met, the data will be flagged as being invalid. 
+#' 
+#' @param data A data frame which has been parsed by either d2Parser or sims2Parser
+#' @return Returns a data frame of invalid period-mechanism combinations. Returns TRUE if there are no violations. 
+#' 
+#' 
+checkMechanismValidity <- function(data, organisationUnit=NA) {
+  
+  if (is.na(organisationUnit)) {
+    organisationUnit = getOption("organisationUnit")
+  }
+  
+  data_mechs_periods <- data %>%
+      dplyr::select(attributeOptionCombo,period) %>%
+    dplyr::distinct()
+  
+  period_info <-
+    do.call(rbind.data.frame, lapply(unique(data_mechs_periods$period), getPeriodFromISO))
+  
+  data_mechs_periods <- merge(data_mechs_periods, period_info, by.x = "period", by.y = "iso")
+  
+  mechanism_map <-
+    getMechanismsMap(organisationUnit = organisationUnit)%>%
+    dplyr::select(id,code,startDate,endDate)
+  
+  names(mechanism_map) <-
+    c("attributeOptionCombo",
+      "code",
+      "startDate_mech",
+      "endDate_mech")
+  
+  data_mechs_periods <-
+    dplyr::left_join(data_mechs_periods, mechanism_map, by = "attributeOptionCombo")
+  
+  data_mechs_periods <-data_mechs_periods %>%
+    dplyr::mutate(is_valid = (startDate >= startDate_mech & endDate <= endDate_mech)) %>%
+    dplyr::mutate(is_valid = ifelse(is.na(is_valid),FALSE,is_valid)) %>%
+    dplyr::filter(!is_valid) 
+    
+
+  if (NROW(data_mechs_periods) > 0) {
+    
+    warning("Invalid mechanisms found!")
+    return(data_mechs_periods)
+  
+    } else {
+    return(TRUE)
+  }
+  
+}
