e7774e3b120666626459f55692294079c84d1efc
Jason Pickering <jason.p.pickering@gmail.com>
2018-12-27 16:23:10 +0100

    Add variables to quiet package checks

---
+++ R/datimvalidation.R
@@ -0,0 +1,31 @@
+#' @docType package
+#' @name datimvalidation
+NULL
+
+
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(
+    c(
+      "orgUnit",
+      "attributeOptionCombo",
+      "period",
+      ".",
+      "categoryOptionCombo",
+      "categoryoptioncombouid",
+      "code",
+      "count",
+      "dataElement",
+      "dataelementuid",
+      "ds",
+      "endDate",
+      "endDate_mech",
+      "id",
+      "is_valid",
+      "is_valid_value",
+      "n",
+      "optionSet.id",
+      "startDate",
+      "startDate_mech",
+      "value"
+    )
+  )
