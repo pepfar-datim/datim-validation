6796331457390f2b6a6368c9650b829174783631
Jason Pickering <jason.p.pickering@gmail.com>
2019-03-22 12:08:40 +0200

    Reformat validateData

--- R/validateData.R
+++ R/validateData.R
@@ -1,10 +1,10 @@
 #' @export
 #' @title Internal function which prepares a parsed data frame for validation
-#' 
+#'
 #' @param d Data frame parsed by d2Parser
 #'
-#' @return Modifed data object with combis and totals appended. 
-#  
+#' @return Modifed data object with combis and totals appended.
+#
 
 prepDataForValidation <- function(d) {
   header <-
@@ -14,15 +14,21 @@ prepDataForValidation <- function(d) {
       "orgUnit",
       "categoryOptionCombo",
       "attributeOptionCombo",
-      "value")
+      "value"
+    )
   
   d <- d[, header[header %in% names(d)]]
-  invalid<-function(x) { sapply(x, function(x) {is.na(x) || missing(x) || x=="" })}  
-  d$value<-as.numeric(d$value) #This may throw a warning 
+  invalid <-
+    function(x) {
+      sapply(x, function(x) {
+        is.na(x) || missing(x) || x == ""
+      })
+    }
+  d$value <- as.numeric(d$value) #This may throw a warning
   
-  total_rows<-NROW(d)
-  d <- d %>% 
-  dplyr::filter( purrr::reduce(purrr::map(., is.na), `+`) == 0 )
+  total_rows <- NROW(d)
+  d <- d %>%
+    dplyr::filter(purrr::reduce(purrr::map(., is.na), `+`) == 0)
   
   if (total_rows != NROW(d)) {
     foo <- total_rows - NROW(d)
@@ -31,30 +37,35 @@ prepDataForValidation <- function(d) {
             " rows are incomplete. Please check your file to ensure its correct.")
     warning(msg)
   }
-
+  
   #Calculate the totals
-  d$combi<-paste0(d$dataElement,".",d$categoryOptionCombo)
-  data.totals<-aggregate(value ~ dataElement + period + orgUnit + attributeOptionCombo, data = d,FUN=sum)
-  data.totals$combi<-data.totals$dataElement
-  data.totals$categoryOptionCombo<-NA
-  data.totals<-data.totals[,names(d)]
-  dplyr::bind_rows(d,data.totals) %>%
-    dplyr::filter(value != 0 )
+  d$combi <- paste0(d$dataElement, ".", d$categoryOptionCombo)
+  data.totals <-
+    aggregate(
+      value ~ dataElement + period + orgUnit + attributeOptionCombo,
+      data = d,
+      FUN = sum
+    )
+  data.totals$combi <- data.totals$dataElement
+  data.totals$categoryOptionCombo <- NA
+  data.totals <- data.totals[, names(d)]
+  dplyr::bind_rows(d, data.totals) %>%
+    dplyr::filter(value != 0)
 }
 
 #' @export
 #' @importFrom stats aggregate
 #' @importFrom stats complete.cases
 #' @title Function which checks the of a DATIM data payload agaist define validation rules
-#' 
+#'
 #' @description validateData should be supplied a d2Parser compliant data frame.
 #'The data frame is checked dynamically against validation rules defined in the DATIM server.
 #' @param data d2Parser data frame
 #' @param organisationUnit Organisation unit. Defaults to the user organisation unit if not supplied.
 #' @param return_violations_only Paramater to return only violations or all validation rule evalualtions.
-#' @param parallel Should the rules be evaluated in parallel. Default is to not evaluate in parallel. 
-#' @param datasets Vector of dataset UIDs which can  be used to restrict 
-#' the validation rules which will be applied. 
+#' @param parallel Should the rules be evaluated in parallel. Default is to not evaluate in parallel.
+#' @param datasets Vector of dataset UIDs which can  be used to restrict
+#' the validation rules which will be applied.
 #' @return Returns a data frame with validation rule results.
 #' @examples \dontrun{
 #'   d<-d2Parser("myfile.csv",type="csv")
@@ -64,8 +75,13 @@ prepDataForValidation <- function(d) {
 #'   ds<-getCurrentMERDataSets()
 #'   vr_rules<-validateData(d,parallel=TRUE,datasets=ds)
 #' }
-validateData<-function(data,organisationUnit=NA,return_violations_only=TRUE,parallel=FALSE,datasets=NA) {
-
+validateData <-
+  function(data,
+           organisationUnit = NA,
+           return_violations_only = TRUE,
+           parallel = FALSE,
+           datasets = NA) {
+    
   if (nrow(data) == 0 ||
       is.null(data)) {
     stop("Data values cannot be empty!")
@@ -94,24 +110,39 @@ validateData<-function(data,organisationUnit=NA,return_violations_only=TRUE,para
 data<-prepDataForValidation(data)
 
 #Empty data frame
-validation.results_empty<-data.frame(name=character(),id=character(),
-                                     periodType=character(),description=character(),
-                                     operator=character(),leftSide.expression=numeric(),
-                                     leftSide.missingValueStrategy=character(),rightSide.expression=numeric(),
-                                     rightSide.ops=integer(),leftSide.ops=integer(),leftSide.count=integer(),
-                                     rightSide.count=integer(),formula=character(),result=logical())
-validation.results<-validation.results_empty
+validation.results_empty <- data.frame(
+  name = character(),
+  id = character(),
+  periodType = character(),
+  description = character(),
+  operator = character(),
+  leftSide.expression = numeric(),
+  leftSide.missingValueStrategy = character(),
+  rightSide.expression = numeric(),
+  rightSide.ops = integer(),
+  leftSide.ops = integer(),
+  leftSide.count = integer(),
+  rightSide.count = integer(),
+  formula = character(),
+  result = logical()
+)
+validation.results <- validation.results_empty
 
 #Check the data against the validation rules
-vr<-getValidationRules()
-if (Sys.info()[['sysname']] == "Windows"  ) { 
-    if  ( parallel == TRUE )  {warning("Parallel execution is not supported on Windows" ); parallel = FALSE }
+vr <- getValidationRules()
+if (Sys.info()[['sysname']] == "Windows") {
+  if (parallel == TRUE)  {
+    warning("Parallel execution is not supported on Windows")
+    parallel = FALSE
+  }
 }
 
-validation.results<-plyr::ddply(data,plyr::.(period,attributeOptionCombo,orgUnit),
-                                    function(x) evaluateValidation(x$combi,x$value,vr,return_violations_only),
-                                    .parallel=parallel,
-                                    .inform=TRUE)
+validation.results <-
+  plyr::ddply(data, plyr::.(period, attributeOptionCombo, orgUnit),
+              function(x)
+                evaluateValidation(x$combi, x$value, vr, return_violations_only),
+              .parallel = parallel,
+              .inform = TRUE)
 
 if ( nrow(validation.results) > 0 ) {
   validation.results <- plyr::colwise(as.character)(validation.results)
