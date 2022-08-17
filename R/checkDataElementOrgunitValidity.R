b01df53016242c1c35ff1e1ec6bea6d859e1c69c
Jason P. Pickering <jason.p.pickering@gmail.com>
2022-08-17 13:11:57 +0200

    Deconflict getDataSets (WIP)

--- R/checkDataElementOrgunitValidity.R
+++ R/checkDataElementOrgunitValidity.R
@@ -1,18 +1,18 @@
 #' @export
 #' @title getDataElementsOrgunits(data,organisationUnit,datasets)
-#' 
+#'
 #' @description Returns a map of lists consisting of data elements and orgunits
 #'  for a dataset (or datasets) for a given organisationUnit
 
 #' @param organisationUnit Organisation unit. Defaults to user organisation
 #'  unit if not supplied explicitly.
-#' @param datasets Should be a character vector of data set UIDs. Alternatively, 
+#' @param datasets Should be a character vector of data set UIDs. Alternatively,
 #' if left missing, user will be prompted.
 #' @param d2session datimutils login session object
 #' @return A named list of data frames, each consisting of two columns (des) representing
 #' data elements and (ous) representing organisation unit UIDs
-#' 
-#' @examples 
+#'
+#' @examples
 #'  \dontrun{
 #'  #Be sure you login to DATIM first
 #'  loadSecrets()
@@ -27,23 +27,23 @@ getDataElementsOrgunits <- function(
   if (is.na(organisationUnit)) {
     organisationUnit = d2session$user_orgunit
   }
-  
-  allDataSets <- getDataSets(d2session = d2session)
-  
+
+  allDataSets <- getDataSetsFromDATIM(d2session = d2session)
+
   if ( length(datasets) == 0 | any(is.na(datasets)) ) {
     datasets <- selectDataset(d2session)
   }
-  
+
   dataSetValid <- Reduce("&", datasets %in% allDataSets$id)
-  
+
   if (!dataSetValid) {
     stop("Invalid dataset")
   }
-  
+
     for (i in seq_along(datasets)) {
-  
+
       if (i == 1)  { des_ous.all <- list() }
-      
+
       url <-
         paste0(
           d2session$base_url,
@@ -52,10 +52,10 @@ getDataElementsOrgunits <- function(
           "&filter=dataSets.id:eq:",
           datasets[i]
         )
-      
+
       sig <- digest::digest(paste0(url), algo = 'md5', serialize = FALSE)
       des_ous <- getCachedObject(sig)
-      
+
       if (is.null(des_ous)) {
         r <- httr::GET(url, httr::timeout(300), handle = d2session$handle)
         r <- httr::content(r, "text")
@@ -67,7 +67,7 @@ getDataElementsOrgunits <- function(
         des_ous <- list(dataset = datasets[i], list(ous = ous, des = des))
         saveCachedObject(des_ous, sig)
       }
-      
+
       des_ous.all <- rlist::list.append(des_ous.all, des_ous)
     }
 
@@ -77,20 +77,20 @@ getDataElementsOrgunits <- function(
 
 #' @export
 #' @title checkDataElementOrgunitValidity(data,organisationUnit,datasets)
-#' 
-#' @description Returns a data frame invalid data elements which exist in the data 
-#' but which do not have a valid organistion unit / dataset association. 
+#'
+#' @description Returns a data frame invalid data elements which exist in the data
+#' but which do not have a valid organistion unit / dataset association.
 #'
 #' @param data D2 compliant data frame
-#' @param organisationUnit Should be the UID of the organisation unit ancestor, 
-#' typically the operating unit. 
-#' @param datasets Should be a character vector of data set UIDs. 
+#' @param organisationUnit Should be the UID of the organisation unit ancestor,
+#' typically the operating unit.
+#' @param datasets Should be a character vector of data set UIDs.
 #' Alternatively, if left missing, user will be promted.
 #' @param return_violations Return the invalid data if TRUE
 #' @param d2session datimutils d2session object @param d2session datimutils d2session object
 #' @return Returns subset of data which contains
 #'   invalid data element / organisation unit associations. If no violations are found, a boolean
-#'   TRUE value is returned. 
+#'   TRUE value is returned.
 #' @examples \dontrun{
 #'      d<-d2Parser("myfile.csv",type="csv")
 #'      ds<-getCurrentMERDataSets(type="RESULTS")
@@ -98,11 +98,11 @@ getDataElementsOrgunits <- function(
 #' }
 
 checkDataElementOrgunitValidity<-function(data=NA,organisationUnit=NA,datasets=NA,return_violations=TRUE, d2session = d2_default_session) {
-  
+
   if (is.na(organisationUnit)) { organisationUnit = d2session$user_orgunit}
   if ( NROW(data) == 0  ) {stop("Data cannot be missing!")}
   if ( length(datasets) == 0 | any(is.na(datasets)) ) { stop("Please specifiy a list of data sets!") }
-  
+
   des_ous<-getDataElementsOrgunits(organisationUnit,datasets,d2session = d2session)
   des_ous_map<-plyr::ldply(des_ous,function(x) expand.grid(dataElement=x[[2]]$des,orgUnit=x[[2]]$ous,stringsAsFactors = FALSE))
   data_des_ous_map<-unique(data[,c("dataElement","orgUnit")])
--- R/checkValueTypeCompliance.R
+++ R/checkValueTypeCompliance.R
@@ -1,26 +1,26 @@
 #' @export
 #' @importFrom dplyr %>%
 #' @title checkValueTypeCompliance(d)
-#' 
+#'
 #' @description This function will check data to ensure that its value matches
 #' a given pattern based on its value type. As an example, if the data element
 #' which is being validated should only accept positive integers, any values
 #' which are not positive integers will be flagged. For values which are composed
-#' of option sets, any values which do not correspond so one of the option set 
+#' of option sets, any values which do not correspond so one of the option set
 #' value codes, will also be flagged. Additionally ,any values which are zero
-#' for which the data element is not zero significant will also be flagged. 
+#' for which the data element is not zero significant will also be flagged.
 #'
 #' @param d A D2Parsed data frame
 #' @param d2session datimutils d2session object
 #' @return Returns a data frame of invalid data only
-#' @examples 
+#' @examples
 #' \dontrun{
 #' d<-d2Parser("myfile.csv",type="csv")
 #' checkValueTypeCompliance(d)
 #' }
-#' 
+#'
 checkValueTypeCompliance<-function(d, d2session = d2_default_session) {
-  
+
   #There are differences in the API version, so first, we need to know which version we are dealing with
   url<-URLencode(paste0(d2session$base_url,"api/",api_version(),"/system/info"))
   r<-httr::GET(url,httr::timeout(300),handle = d2session$handle)
@@ -34,32 +34,32 @@ checkValueTypeCompliance<-function(d, d2session = d2_default_session) {
          INTEGER_ZERO_OR_POSITIVE="(^0$)|(^[1-9]\\d*$)",
          INTEGER_NEGATIVE = "^-[1-9]\\d*$",
          ZERO_PATTERN = "^0(\\.0*)?$",
-         BOOLEAN="^(true|false|True|False|0|1)$",
-         TRUE_ONLY="^(true|True|1)$",
+         BOOLEAN="^(true|false|True|False|TRUE|FALSE|0|1)$",
+         TRUE_ONLY="^(true|True|TRUE|1)$",
          PERCENTAGE="^([0-9]|[1-9][0-9]|100)(\\.[0-9]+)?$",
          UNIT_INTERVAL="^(0(\\.[0-9]+)?)$|^1$",
          DATE="^(19|20)\\d\\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$",
          DATETIME="^(19|20)\\d\\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-4]):([0-5][0-9]):([0-9][0-9])(\\.\\d{2,3})?$")
-  
+
   patterns<-reshape2::melt(patterns)
-  names(patterns)<-c("regex","valueType")    
+  names(patterns)<-c("regex","valueType")
 
   des<-getDataElementMap(d2session = d2session)
   des<-merge(des,patterns,by="valueType",all.x=T)
   d<-merge(d,des,by.x="dataElement",by.y="id")
-  
+
   #Support only valueTypes with a regex
   d_regex_validation<-d[!is.na(d$regex),]
-  
+
   if (NROW(d_regex_validation) > 0)  {
-  
+
   d_regex_validation$is_valid_pattern <-
     mapply(grepl,
            d_regex_validation$regex,
            as.character(d_regex_validation$value))
-  
-  #Validation of zero significance. If the value is zero and the the 
-  #data element is not zero significant, then flag it. 
+
+  #Validation of zero significance. If the value is zero and the the
+  #data element is not zero significant, then flag it.
   #TRUE on Non-zeros and valid zeros. False on invalid zeros
   d_regex_validation$is_valid_zero<- !(d_regex_validation$value == "0" & !d_regex_validation$zeroIsSignificant)
   d_regex_validation$is_valid_value <- d_regex_validation$is_valid_pattern  &  d_regex_validation$is_valid_zero
@@ -89,18 +89,18 @@ checkValueTypeCompliance<-function(d, d2session = d2_default_session) {
 
 #' @export
 #' @title getOptionSetMap()
-#' 
+#'
 #' @description Utility function to fetch a list of optionsets and possible values within them
 #' @param d2session datimutils d2session object
-#' @return Returns a data frame structure composed of option set names, UIDs, and a data frame for 
+#' @return Returns a data frame structure composed of option set names, UIDs, and a data frame for
 #' each of these with individual options present in the option set.
-#' @examples 
+#' @examples
 #'  \dontrun{
 #'   op_set_map<-getOptionSetMap()
 #' }
 getOptionSetMap<-function(d2session = d2_default_session) {
 
-  
+
   url<-URLencode(paste0(d2session$base_url,"api/",api_version(),"/optionSets?fields=id,name,options[code]&paging=false"))
   sig<-digest::digest(paste0(url),algo='md5', serialize = FALSE)
   option_sets<-getCachedObject(sig)
@@ -111,39 +111,39 @@ getOptionSetMap<-function(d2session = d2_default_session) {
       r<- jsonlite::fromJSON(r,flatten = TRUE)
       option_sets<-as.data.frame(r$optionSets)
       saveCachedObject(option_sets,sig)}
-    
+
     else {stop("Could not get a list of option sets")}
   }
-  
+
   return(option_sets)
 }
 
 
 #' @export
 #' @title checkOptionSetCompliance()
-#' 
+#'
 #' @param d D2Parsed data frame
 #' @param d2session datimutils d2session object
 #' @description Internal function for validation of data which have option sets
 #'
 #' @return Returns a data frame of invalid values validated against their option set.
-#' 
-#' @examples 
+#'
+#' @examples
 #'  \dontrun{
 #'   d<-d2Parser("myfile.csv",type="csv")
 #'   checkOptionSetCompliance(d)
 #' }
 checkOptionSetCompliance<-function(d, d2session = d2_default_session) {
-  
-  option_sets_des<-getDataElementMap(d2session = d2session) %>% 
+
+  option_sets_des<-getDataElementMap(d2session = d2session) %>%
     dplyr::filter(!is.na(optionSet.id)) %>%
     dplyr::select(dataElement=id,optionSetID=optionSet.id) %>%
     dplyr::distinct()
-  
-  d <-  dplyr::inner_join(d,option_sets_des,by="dataElement") 
-  
+
+  d <-  dplyr::inner_join(d,option_sets_des,by="dataElement")
+
   if (NROW(d) == 0) {
-    
+
     foo<-data.frame(dataElement=character(),
                period=character(),
                categoryOptionCombo=character(),
@@ -153,17 +153,17 @@ checkOptionSetCompliance<-function(d, d2session = d2_default_session) {
                stringsAsFactors = FALSE)
     return(foo)
   }
-  
+
   option_set_map<-getOptionSetMap(d2session = d2session)
-  
+
   getOptionSetValues<-function(x) {
     list_index<-which(option_set_map$id == x)
     option_set_map[list_index,"options"][[1]] %>% dplyr::pull("code")
   }
-  
+
   d$option_set_values_list<-lapply(d$optionSetID, getOptionSetValues)
   d$is_valid_value<-mapply(function(x,y) x %in% y, d$value,d$option_set_values_list)
   d %>% dplyr::select(dataElement,period,orgUnit,categoryOptionCombo,attributeOptionCombo,value,is_valid_value) %>%
     dplyr::filter(!is_valid_value)
-    
+
 }
