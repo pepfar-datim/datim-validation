7ed3ed426a73c78a570103ffc5b3242a14d2bb49
Jason Pickering <jason.p.pickering@gmail.com>
2022-11-28 09:26:39 +0100

    Simiplification

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -1,20 +1,12 @@
 test_that("Can pass a valid validation rule expression", {
 
-  inds <- "#{AppEXdI1dhS}"
+  inds <- list("#{AppEXdI1dhS}",
+               "#{AppEXdI1dhS} + 10",
+               "( 2 * #{P3jJH5Tu5VC.S34ULMcHMca} ) / ( #{FQ2o8UBlcrS.S34ULMcHMca} - 200 ) * 25")
+
   parsed_inds <- lapply(inds, function(x) lex(x, indicator_regexes))
   is_valid <- isValidIndicator(parsed_inds)
-  expect_true(is_valid)
-
-  inds <- "#{AppEXdI1dhS} + 10"
-  parsed_inds <- lapply(inds, function(x) lex(x, indicator_regexes))
-  is_valid <- isValidIndicator(parsed_inds)
-  expect_true(all(is_valid))
-
-
-  inds <- "( 2 * #{P3jJH5Tu5VC.S34ULMcHMca} ) / ( #{FQ2o8UBlcrS.S34ULMcHMca} - 200 ) * 25"
-  parsed_inds <- lapply(inds, function(x) lex(x, indicator_regexes))
-  is_valid <- isValidIndicator(parsed_inds)
-  expect_true(is_valid)
+  expect_identical(is_valid, c(TRUE, TRUE, TRUE))
 
 })
 
