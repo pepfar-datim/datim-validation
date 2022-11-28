90a2fd12ec19a1d3b869f29f092025554b55d704
Jason Pickering <jason.p.pickering@gmail.com>
2022-11-28 09:28:45 +0100

    Remove duplicative test

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -2,11 +2,12 @@ test_that("Can pass a valid validation rule expression", {
 
   inds <- list("#{AppEXdI1dhS}",
                "#{AppEXdI1dhS} + 10",
-               "( 2 * #{P3jJH5Tu5VC.S34ULMcHMca} ) / ( #{FQ2o8UBlcrS.S34ULMcHMca} - 200 ) * 25")
+               "( 2 * #{P3jJH5Tu5VC.S34ULMcHMca} ) / ( #{FQ2o8UBlcrS.S34ULMcHMca} - 200 ) * 25",
+               "1+2+3-4")
 
   parsed_inds <- lapply(inds, function(x) lex(x, indicator_regexes))
   is_valid <- isValidIndicator(parsed_inds)
-  expect_identical(is_valid, c(TRUE, TRUE, TRUE))
+  expect_identical(is_valid, c(TRUE, TRUE, TRUE,TRUE))
 
 })
 
